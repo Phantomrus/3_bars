77a15e4b0dbf34fa431d8abbd726cf70cbaed9eb
Phantomrus <phantomrus@gmail.com>
2016-10-04 17:20:55 +0300

    Изменен механизм поиска max и min значений
    В функциях get_biggest_bar, get_smallest_bar, get_closest_bar поиск максимальных и минимальных значений во вложенных словарях реализован с помощью стандартных возможностей функций max() и min().
    
    Имя функции get_closest_bar изменено на get_nearest_bar для логического соответствия остальному коду программы.

--- bars.py
+++ bars.py
@@ -8,29 +8,28 @@ def load_data(filepath):
     return bars_description
 
 def get_biggest_bar(data):
-    bars_size = []
-    for bar_list_id, bar in enumerate(bars_description): # Перебираем бары по одному.
-        bars_size.append([bar['Cells']['SeatsCount'], bar_list_id])
-    return max(bars_size)[1]
+    biggest_bar = max(data, key=lambda x: x['Cells']['SeatsCount'])    
+    return biggest_bar
 
 
 def get_smallest_bar(data):
-    bars_size = []
-    for bar_list_id, bar in enumerate(bars_description): # Перебираем бары по одному.
-        bars_size.append([bar['Cells']['SeatsCount'], bar_list_id])
-    return min(bars_size)[1]
+    smallest_bar = min(data, key=lambda x: x['Cells']['SeatsCount'])    
+    return smallest_bar
 
-
-def get_closest_bar(bars_description, latitude, longitude):
+def get_nearest_bar(data, latitude, longitude):
+'''
     bars_distance = [] # Первым элементом каждого вложенного массива будет дистанция, вторым - id.
-    for bar_list_id, bar in enumerate(bars_description): # Перебираем бары по одному.
+    for bar_list_id, bar in enumerate(data): # Перебираем бары по одному.
         distance_to_current_bar = \
         math.sqrt((bar['Cells']['geoData']['coordinates'][0] - float(latitude))**2 +
             (bar['Cells']['geoData']['coordinates'][1] - float(longitude))**2)
             # Используем теорему Пифагора, принимая, что в пределах Москвы можем считать, что работаем на плоскости, а не на эллипсоиде/геоиде.
         bars_distance.append([distance_to_current_bar, bar_list_id])
-    return min(bars_distance)[1]
+    return min(bars_distance)[1]'''
     
+    nearest_bar = min(data, key=lambda x: math.sqrt((x['Cells']['geoData']['coordinates'][0] - float(latitude))**2 +
+            (x['Cells']['geoData']['coordinates'][1] - float(longitude))**2))
+    return nearest_bar
 
 
 if __name__ == '__main__':
@@ -40,23 +39,23 @@ if __name__ == '__main__':
 
     latitude, longitude = input('Введите ваши GPS-координаты через пробел в формате \
                         \"55.754069 37.620543\"\n').split()
-    nearest_bar_id = get_closest_bar(bars_description, latitude, longitude)
+    nearest_bar = get_nearest_bar(bars_description, latitude, longitude)
     print("\n\nБлижайший к тебе бар:\n%s\nID: %s\nКоординаты: %s\nКоличество мест: %s\n" %
-       (bars_description[nearest_bar_id]['Cells']['Name'],
-        bars_description[nearest_bar_id]['Id'],
-        bars_description[nearest_bar_id]['Cells']['geoData']['coordinates'],
-        bars_description[nearest_bar_id]['Cells']['SeatsCount']))
+       (nearest_bar['Cells']['Name'],
+        nearest_bar['Id'],
+        nearest_bar['Cells']['geoData']['coordinates'],
+        nearest_bar['Cells']['SeatsCount']))
 
-    biggest_bar_id = get_biggest_bar(bars_description)
+    biggest_bar = get_biggest_bar(bars_description)
     print("Самый большой бар:\n%s\nID: %s\nКоординаты: %s\nКоличество мест: %s\n" %
-       (bars_description[biggest_bar_id]['Cells']['Name'],
-        bars_description[biggest_bar_id]['Id'],
-        bars_description[biggest_bar_id]['Cells']['geoData']['coordinates'],
-        bars_description[biggest_bar_id]['Cells']['SeatsCount']))
+       (biggest_bar['Cells']['Name'],
+        biggest_bar['Id'],
+        biggest_bar['Cells']['geoData']['coordinates'],
+        biggest_bar['Cells']['SeatsCount']))
 
-    smallest_bar_id = get_smallest_bar(bars_description)
+    smallest_bar = get_smallest_bar(bars_description)
     print("Самый маленький бар:\n%s\nID: %s\nКоординаты: %s\nКоличество мест: %s\n" %
-       (bars_description[smallest_bar_id]['Cells']['Name'],
-        bars_description[smallest_bar_id]['Id'],
-        bars_description[smallest_bar_id]['Cells']['geoData']['coordinates'],
-        bars_description[smallest_bar_id]['Cells']['SeatsCount']))
+       (smallest_bar['Cells']['Name'],
+        smallest_bar['Id'],
+        smallest_bar['Cells']['geoData']['coordinates'],
+        smallest_bar['Cells']['SeatsCount']))
